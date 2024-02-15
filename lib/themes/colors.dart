e333987377ab6dd0b05718882388b71c50a4b405
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-02-15 18:04:34 +0200

    Created themes folder

---
+++ lib/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyAppColors {
+  static const Color backGroundColor = Color(0xFF121417);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color containerColor = Color(0xFF293038);
+  static const Color accentColor = Color(0xFF9CABBA);
+}
