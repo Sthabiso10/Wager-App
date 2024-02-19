bb06bd8af2f3c569b37b562000324d8f1b7f5647
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-02-19 14:58:00 +0200

    Create bet_tile.dart

---
+++ lib/utils/bet_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BetTile extends StatelessWidget {
+  const BetTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
