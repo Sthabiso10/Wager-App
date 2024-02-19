7b3606eed247ec9227023e35701f2f4cb025c70d
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-02-19 18:37:44 +0200

    Update settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import '../themes/colors.dart';
 
-class _SettingsPagState extends StatefulWidget {
-  const _SettingsPagState({super.key});
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
 
   @override
-  State<_SettingsPagState> createState() => __SettingsPagStateState();
+  _SettingsPageState createState() => _SettingsPageState();
 }
 
-class __SettingsPagStateState extends State<_SettingsPagState> {
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MyAppColors.backGroundColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: MyAppColors.accentColor,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Settings Page',
+          style: TextStyle(
+            color: MyAppColors.textColor,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
   }
 }
