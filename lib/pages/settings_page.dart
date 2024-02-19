c97291754ea1c0cf1aa07200abeb77feb9f4beb6
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-02-19 17:07:33 +0200

    Create settings_page.dart

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _SettingsPagState extends StatefulWidget {
+  const _SettingsPagState({super.key});
+
+  @override
+  State<_SettingsPagState> createState() => __SettingsPagStateState();
+}
+
+class __SettingsPagStateState extends State<_SettingsPagState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
