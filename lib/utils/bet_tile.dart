206f28f04f1eb592449f809b30606fd9cc977599
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-02-29 12:00:04 +0200

    Update bet_tile.dart

--- lib/utils/bet_tile.dart
+++ lib/utils/bet_tile.dart
@@ -11,65 +11,72 @@ class BetTile extends StatelessWidget {
       {super.key,
       required this.betTitle,
       required this.betDescription,
-      required this.betAmount});
+      required this.betAmount,
+      required this.selectedBet});
+
+  final void Function() selectedBet;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20.0),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-        decoration: BoxDecoration(
-          color: MyAppColors.containerColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: selectedBet,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          decoration: BoxDecoration(
+            color: MyAppColors.containerColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      betTitle,
+                      style: GoogleFonts.workSans(
+                          color: MyAppColors.textColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      betDescription,
+                      style: GoogleFonts.workSans(
+                          color: MyAppColors.textColor, fontSize: 16),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              Row(
                 children: [
                   Text(
-                    betTitle,
+                    "R",
                     style: GoogleFonts.workSans(
                         color: MyAppColors.textColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500),
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    betDescription,
+                    betAmount,
                     style: GoogleFonts.workSans(
-                        color: MyAppColors.textColor, fontSize: 16),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
+                        color: MyAppColors.textColor,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700),
                   )
                 ],
               ),
-            ),
-            const SizedBox(
-              width: 30,
-            ),
-            Row(
-              children: [
-                Text(
-                  "R",
-                  style: GoogleFonts.workSans(
-                      color: MyAppColors.textColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  betAmount,
-                  style: GoogleFonts.workSans(
-                      color: MyAppColors.textColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w700),
-                )
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
