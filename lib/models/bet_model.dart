7a87e5c17996a3675d234f28d678567a6bb13beb
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-03-06 22:22:12 +0200

    Create bet_model.dart

---
+++ lib/models/bet_model.dart
@@ -0,0 +1,16 @@
+class Bet {
+  final String id;
+  final String title;
+  final String description;
+  final double amount;
+
+  //bool isAccepted;
+  //String acceptedBy;
+
+  Bet({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.amount,
+  });
+}
