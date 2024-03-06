43e8484e609a5a727af2d7fad9fe75b710c1ca79
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-03-06 22:22:09 +0200

    Create dummy_data.dart

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,23 @@
+import '../models/bet_model.dart';
+
+final List<Bet> dummyBet = [
+  Bet(
+    id: '1',
+    title: 'Basketball Match Bet',
+    description: 'Bet on the outcome of the basketball match.',
+    amount: 30.0,
+    // March 15, 2024, 6:00 PM
+  ),
+  Bet(
+    id: '2',
+    title: 'Soccer Match Bet',
+    description: 'Bet on the outcome of the soccer match.',
+    amount: 20.0,
+  ),
+  Bet(
+    id: '3',
+    title: 'Tennis Match Bet',
+    description: 'Bet on the outcome of the tennis match.',
+    amount: 50.0,
+  ),
+];
