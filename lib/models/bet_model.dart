02f1909c6f265bcf38943fe5158f546709f1aec0
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-03-07 02:55:01 +0200

    Update bet_model.dart

--- lib/models/bet_model.dart
+++ lib/models/bet_model.dart
@@ -1,16 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
+
+const uuid = Uuid();
+
 class Bet {
+  Bet({
+    required this.date,
+    required this.title,
+    required this.description,
+    required this.amount,
+  }) : id = uuid.v4();
   final String id;
   final String title;
   final String description;
   final double amount;
+  final DateTime date;
 
-  //bool isAccepted;
-  //String acceptedBy;
-
-  Bet({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.amount,
-  });
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
