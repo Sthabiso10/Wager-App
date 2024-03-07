99c46c4b3d9ee179ef9035b1541dd1c6b85549b7
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-03-07 02:54:56 +0200

    Update dummy_data.dart

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -2,20 +2,19 @@ import '../models/bet_model.dart';
 
 final List<Bet> dummyBet = [
   Bet(
-    id: '1',
+    date: DateTime.now(),
     title: 'Basketball Match Bet',
     description: 'Bet on the outcome of the basketball match.',
     amount: 30.0,
-    // March 15, 2024, 6:00 PM
   ),
   Bet(
-    id: '2',
+    date: DateTime.now(),
     title: 'Soccer Match Bet',
     description: 'Bet on the outcome of the soccer match.',
     amount: 20.0,
   ),
   Bet(
-    id: '3',
+    date: DateTime.now(),
     title: 'Tennis Match Bet',
     description: 'Bet on the outcome of the tennis match.',
     amount: 50.0,
