17bd7c9dd569baab91a6367909e27b0b009e642d
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2025-11-08 00:19:21 +0200

    Create login_or_register.dart

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:wager_app/app/login/login_view/login_view.dart';
+import 'package:wager_app/app/register/register_view/register_view.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  // show login page first
+  bool showLoginPage = true;
+
+  //toggle between login and register page
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginView(
+        onTap: togglePages,
+      );
+    } else {
+      return RegisterView(
+        onTap: togglePages,
+      );
+    }
+  }
+}
