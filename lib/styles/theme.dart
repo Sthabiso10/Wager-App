896951dff92dbd06b6f7bfeaa95a4ba74517659a
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2025-11-07 16:19:03 +0200

    Create theme.dart

---
+++ lib/styles/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'colors.dart';
+
+appTheme({required TextTheme textTheme}) {
+  return ThemeData(
+      //scaffoldBackgroundColor: kColorBackground,
+      textTheme: GoogleFonts.ubuntuTextTheme(textTheme).copyWith(
+          bodyMedium: GoogleFonts.ubuntu(textStyle: textTheme.bodyMedium)),
+      primaryColor: backgroundColor,
+      colorScheme: ColorScheme.fromSeed(seedColor: colorAccent),
+      appBarTheme: AppBarTheme(
+        backgroundColor: backgroundColor,
+        actionsIconTheme: IconThemeData(color: colorText),
+      ),
+      dialogTheme: const DialogThemeData(
+        elevation: 8,
+        //backgroundColor: kColorBackground,
+      ),
+      dividerColor: Colors.transparent);
+}
