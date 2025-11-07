e94e3c016ef60047555614e2a61e4cbc748e78ff
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2025-11-07 16:19:45 +0200

    Create settings_view.dart

---
+++ lib/app/settings/views/settings_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../../../styles/colors.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: colorAccent,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Settings Page',
+          style: TextStyle(
+            color: colorText,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
