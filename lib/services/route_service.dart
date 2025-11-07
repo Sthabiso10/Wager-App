238752f831b86d634e6a6c6ea6a3b45dcc825442
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2025-11-08 00:19:28 +0200

    Update route_service.dart

--- lib/services/route_service.dart
+++ lib/services/route_service.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:wager_app/app/navigation%20bar/views/navigation_view.dart';
 import 'package:wager_app/app/profie/views/profile_view.dart';
+import 'package:wager_app/auth/auth.dart';
 import '../app/home/views/home_view.dart';
 
 class RouteService {
   static const String homeView = '/home';
   static const String profileView = '/profile';
   static const String navigationMenu = '/naviagtion';
+  static const String inbox = '/inbox';
+  static const String authPage = '/auth';
 
   static Route? getRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -16,6 +19,8 @@ class RouteService {
         return MaterialPageRoute(builder: (context) => const ProfileView());
       case navigationMenu:
         return MaterialPageRoute(builder: (context) => NavigationMenu());
+      case authPage:
+        return MaterialPageRoute(builder: (context) => const AuthPage());
 
       default:
         return null;
