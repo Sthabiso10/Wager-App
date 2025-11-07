c69f191543ae6acdf17df25d6bdaa55a8bf7202c
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2025-11-07 16:20:45 +0200

    Create my_button.dart

---
+++ lib/app/global_widgets/my_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:wager_app/styles/colors.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  VoidCallback onPressed;
+  MyButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: colorAccent,
+      child: Text(text),
+    );
+  }
+}
