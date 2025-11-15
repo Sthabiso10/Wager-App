c8f7d75b79bee8cfbeb59c2e00a8bf3eb9ae8250
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2025-11-15 02:07:13 +0200

    Create settings_tile.dart

---
+++ lib/app/profie/widgets/settings_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SettingsTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+
+  const SettingsTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.trailing,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.blue),
+      title: Text(title, style: const TextStyle(fontWeight: FontWeight.w500)),
+      trailing: trailing,
+      onTap: onTap,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+    );
+  }
+}
