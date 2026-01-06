75f4338c6bcae2a0d5d2e58403c99d2969d1af32
Sthabiso Thamsanqa Dlamini <Sthabisod10@gmail.com>
2026-01-06 08:34:28 +0200

    Update route_service.dart

--- lib/services/route_service.dart
+++ lib/services/route_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wager_app/app/add_friends/add_friends_view/add_friends_view.dart';
+import 'package:wager_app/app/create_bet/views';
 import 'package:wager_app/app/inbox/views/inbox_view.dart';
 import 'package:wager_app/app/navigation%20bar/views/navigation_view.dart';
 import 'package:wager_app/app/profie/views/profile_view.dart';
@@ -13,6 +14,7 @@ class RouteService {
   static const String inbox = '/inbox';
   static const String authPage = '/auth';
   static const String friendsView = '/friends';
+  static const String createBet = '/createBet';
 
   static Route? getRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -28,6 +30,8 @@ class RouteService {
         return MaterialPageRoute(builder: (context) => const InboxView());
       case friendsView:
         return MaterialPageRoute(builder: (context) => const AddFriendsView());
+      case createBet:
+        return MaterialPageRoute(builder: (context) => const CreateBetView());
 
       default:
         return null;
